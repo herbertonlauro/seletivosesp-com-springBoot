00474558765cb0c344c983e87876628cffc993a8
Herberton <herberton.dev@gmail.com>
2024-03-18 17:33:11 -0400

    Configurações iniciais

--- src/main/java/com/seletivosespspring/SeletivosespSpringApplication.java
+++ src/main/java/com/seletivosespspring/SeletivosespSpringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SeletivosespSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SeletivosespSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SeletivosespSpringApplication.class, args);
+    }
 
 }
