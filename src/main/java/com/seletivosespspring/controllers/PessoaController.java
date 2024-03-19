d8bf2ff54826d109dcc4a06a75d9e12b64bce6b6
Herberton <herberton.dev@gmail.com>
2024-03-19 17:02:15 -0400

    atualização PessoaController Pessoa

--- src/main/java/com/seletivosespspring/controllers/PessoaController.java
+++ src/main/java/com/seletivosespspring/controllers/PessoaController.java
@@ -1,4 +1,35 @@
 package com.seletivosespspring.controllers;
 
+import com.seletivosespspring.dto.PessoaDTO;
+import com.seletivosespspring.services.PessoaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/pessoa")
 public class PessoaController {
+
+    @Autowired
+    PessoaService pessoaService;
+
+    @GetMapping
+    public List<PessoaDTO> listar(@RequestParam(name = "page", defaultValue = "0") int page,
+                                  @RequestParam(name = "size", defaultValue = "10") int size) {
+        return pessoaService.listarTodasPessoas(page, size);
+    }
+
+    @PostMapping
+    public ResponseEntity<PessoaDTO> criar(@RequestBody PessoaDTO pessoaDTO){
+        PessoaDTO pessoa = pessoaService.salvarPessoa(pessoaDTO);
+        return ResponseEntity.created(URI.create("/pessoa")).body(pessoa);
+    }
+    @PutMapping("{id}")
+    public PessoaDTO atualizar(){
+    return null;
+    }
+
 }
--- src/main/java/com/seletivosespspring/models/Pessoa.java
+++ src/main/java/com/seletivosespspring/models/Pessoa.java
@@ -1,14 +1,20 @@
 package com.seletivosespspring.models;
 
 import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Entity
 @Getter
 @Setter
-@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "pessoa")
 public class Pessoa {
     @Id
