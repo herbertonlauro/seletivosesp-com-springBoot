585a0505b551ec59feab27dceba981b1308597fc
Herberton <herberton.dev@gmail.com>
2024-03-19 17:01:51 -0400

    atualização  EnderecoMapper

--- src/main/java/com/seletivosespspring/mappers/EnderecoMapper.java
+++ src/main/java/com/seletivosespspring/mappers/EnderecoMapper.java
@@ -2,8 +2,9 @@ package com.seletivosespspring.mappers;
 
 import com.seletivosespspring.dto.EnderecoDTO;
 import com.seletivosespspring.models.Endereco;
+import org.mapstruct.Mapper;
 
-@org.mapstruct.Mapper(unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE, componentModel = "spring")
+@Mapper(unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE, componentModel = "spring")
 public interface EnderecoMapper {
     Endereco toEntity(EnderecoDTO enderecoDto);
 
